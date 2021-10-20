d5e72a2519e06b9120d6492acee9793edbf22092
Rob Schrage <rjschrage@gmail.com>
2021-10-20 12:46:36 -0600

    Completed palindrome exercise

---
+++ src/completed-exercises/palindrome/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Main.completed_exercises.Palindrome
+{
+    public class Palindrome
+    {
+        public bool Execute(string inputString)
+        {
+            var inputStringReversed = new string(inputString.Reverse().ToArray());
+            bool isPalindrome;
+
+            if (inputString == inputStringReversed)
+            {
+                isPalindrome = true;
+            }
+            else
+            {
+                isPalindrome = false;
+            }
+
+            return isPalindrome;
+        }
+    }
+}
---
+++ tests/completed-exercises/palindrome/PalindromeTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Main.completed_exercises.Palindrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.Palindrome.Tests
+{
+    [TestFixture()]
+    public class PalindromeTests
+    {
+        [Test()]
+        [TestCase("aba", true)]
+        [TestCase(" aba", false)]
+        [TestCase("aba ", false)]
+        [TestCase("greetings", false)]
+        [TestCase("1000000001", true)]
+        [TestCase("Fish hsif", false)]
+        [TestCase("pennep", true)]
+        public void ExecuteTest(string strToCheck, bool isPalindrome)
+        {
+            var test = new Palindrome();
+            var actual = test.Execute(strToCheck);
+
+            Assert.AreEqual(isPalindrome, actual);
+        }
+    }
+}
