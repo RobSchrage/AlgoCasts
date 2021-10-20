554b71f713a92cbcf22c8093793a895d912298bd
Rob Schrage <rjschrage@gmail.com>
2021-10-20 12:23:51 -0600

    Completed reverse string exercise

---
+++ src/completed-exercises/reversestring/ReverseString.cs
@@ -0,0 +1,49 @@
+﻿// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Main.completed_exercises.reversestring
+{
+    public class ReverseString
+    {
+        //public string Execute(string str)
+        //{
+        //    var arr = str.ToCharArray();
+        //    var reversedString = "";
+
+        //    foreach (var c in arr)
+        //    {
+        //        reversedString = c + reversedString;
+        //    }
+
+        //    return reversedString;
+        //}
+
+        //public string Execute(string str)
+        //{
+        //    var sb = new StringBuilder();
+
+        //    foreach (var c in str)
+        //    {
+        //        sb.Insert(0, c);
+        //    }
+
+        //    return sb.ToString();
+        //}
+
+        public string Execute(string str)
+        {
+            var test = str.Aggregate("", (acc, x) => x + acc);
+
+            return test.ToString();
+        }
+    }
+}
---
+++ tests/completed-exercises/reversestring/ReverseStringTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Main.completed_exercises.reversestring;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.reversestring.Tests
+{
+    [TestFixture()]
+    public class ReverseStringTests
+    {
+        [Test()]
+        [TestCase("abcd", "dcba")]
+        [TestCase("  abcd", "dcba  ")]
+        public void ExecuteTest(string stringToReverse, string expected)
+        {
+            var test = new ReverseString();
+
+            string actual = test.Execute(stringToReverse);
+
+            Assert.AreEqual(expected, actual);
+            //Assert.AreEqual(test.Execute("  abcd"), "dcba  ");
+        }
+    }
+}
