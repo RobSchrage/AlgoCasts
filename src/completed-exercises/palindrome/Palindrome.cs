913bb634680a8507360fd4ce2d8a31b1f5aa43f2
Rob Schrage <rjschrage@gmail.com>
2021-10-20 12:48:33 -0600

    Refactored

--- src/completed-exercises/palindrome/Palindrome.cs
+++ src/completed-exercises/palindrome/Palindrome.cs
@@ -10,17 +10,7 @@ namespace Main.completed_exercises.Palindrome
         public bool Execute(string inputString)
         {
             var inputStringReversed = new string(inputString.Reverse().ToArray());
-            bool isPalindrome;
-
-            if (inputString == inputStringReversed)
-            {
-                isPalindrome = true;
-            }
-            else
-            {
-                isPalindrome = false;
-            }
-
+            bool isPalindrome = inputString == inputStringReversed;
             return isPalindrome;
         }
     }
