1fd06eae88e8730079055707c3b57ca29816541b
Rob Schrage <rjschrage@gmail.com>
2021-10-20 14:43:35 -0600

    Added Palindrome linq solution

--- src/completed-exercises/palindrome/Palindrome.cs
+++ src/completed-exercises/palindrome/Palindrome.cs
@@ -7,11 +7,24 @@ namespace Main.completed_exercises.Palindrome
 {
     public class Palindrome
     {
+        //public bool Execute(string inputString)
+        //{
+        //    var inputStringReversed = new string(inputString.Reverse().ToArray());
+        //    bool isPalindrome = inputString == inputStringReversed;
+        //    return isPalindrome;
+        //}
+
         public bool Execute(string inputString)
         {
-            var inputStringReversed = new string(inputString.Reverse().ToArray());
-            bool isPalindrome = inputString == inputStringReversed;
-            return isPalindrome;
+
+            var matchCount =
+                Enumerable.Range(1, (inputString.Length / 2) + 1)
+                .Count(i =>
+                {
+                    return inputString[i - 1] != inputString[inputString.Length - i];
+                });
+            
+            return matchCount == 0;
         }
     }
 }
