61c69dc5e8521733861a63a263423c88a4c977da
Rob Schrage <rjschrage@gmail.com>
2021-10-20 21:11:34 -0600

    Completed ReverseInt exercise

---
+++ src/completed-exercises/reverseint/ReverseInt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+namespace Main.completed_exercises.reverseint
+{
+    public class ReverseInt
+    {
+        public string Execute(int intToReverse)
+        {
+            var signString = Math.Sign(intToReverse) == -1 ? "-" : "";
+
+            var reverseInt = signString + 
+               ( new string(Math.Abs(intToReverse)
+                .ToString()
+                .Reverse()
+                .ToArray())).TrimStart('0');
+
+            return reverseInt;
+        }
+    }
+}
---
+++ tests/completed-exercises/reverseint/ReverseIntTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Main.completed_exercises.reverseint;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.reverseint.Tests
+{
+    [TestFixture()]
+    public class ReverseIntTests
+    {
+        [Test()]
+        [TestCase(5, "5")]
+        [TestCase(15, "51")]
+        [TestCase(90, "9")]
+        [TestCase(2359, "9532")]
+        [TestCase(-5, "-5")]
+        [TestCase(-15, "-51")]
+        [TestCase(-90, "-9")]
+        [TestCase(-2359, "-9532")]
+        public void ExecuteTest(int intToReverse, string expectedResult)
+        {
+            var test = new ReverseInt();
+            var actual = test.Execute(intToReverse);
+
+            Assert.AreEqual(expectedResult, actual);
+        }
+    }
+}
