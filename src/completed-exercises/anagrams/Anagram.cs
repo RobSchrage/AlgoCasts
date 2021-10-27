3b7443694fb24660e4b90e035313f04779ce58af
Rob Schrage <rjschrage@gmail.com>
2021-10-27 14:40:57 -0600

    Completed anagram

---
+++ src/completed-exercises/anagrams/Anagram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.anagrams
+{
+    public class Anagram
+    {
+        public bool Execute(string stringA, string stringB)
+        {
+            if (stringA.Length != stringB.Length)
+                return false;
+
+            var dictA = new Dictionary<char, int>();
+            var dictB = new Dictionary<char, int>();
+
+            foreach(var c in stringA)
+            {
+                dictA.TryGetValue(c, out var value);
+                dictA[c] = value + 1;
+            }
+
+            foreach (var c in stringB)
+            {
+                dictB.TryGetValue(c, out var value);
+                dictB[c] = value + 1;
+            }
+
+            foreach(var k in dictA.Keys)
+            {
+                if(dictA[k] != dictA[k])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ tests/completed-exercises/anagrams/AnagramTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Main.completed_exercises.anagrams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.anagrams.Tests
+{
+    [TestFixture()]
+    public class AnagramTests
+    {
+        [Test()]
+        public void ExecuteTest()
+        {
+            var test = new Anagram();
+            var actualResult = test.Execute("Helloss", "eHllos");
+
+            Assert.Fail();
+        }
+    }
+}
