2455b38379ede367a03330246c7d88f6c006ad2c
Rob Schrage <rjschrage@gmail.com>
2021-10-27 15:05:11 -0600

    Completed capitalize

---
+++ src/completed-exercises/capitalize/Capitalize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.capitalize
+{
+    public class Capitalize
+    {
+        public string Execute(string inputString)
+        {
+            if (inputString.Length == 0)
+                return "";
+
+            var inputStringArr = inputString.Split(" ");
+
+            var uppercaseStringArray = new List<string>();
+            foreach(var word in inputStringArr)
+            {
+                var firstLetter = word[0].ToString().ToUpper(); ;
+                var restOfWord = word.Substring(1);
+
+                uppercaseStringArray.Add(firstLetter + restOfWord);
+            }
+
+            var capitalizedString = String.Join(" ", uppercaseStringArray);
+            return capitalizedString;
+        }
+
+    }
+}
---
+++ tests/completed-exercises/capitalize/CapitalizeTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Main.completed_exercises.capitalize;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main.completed_exercises.capitalize.Tests
+{
+    [TestFixture()]
+    public class CapitalizeTests
+    {
+        [Test()]
+        public void ExecuteTest()
+        {
+            var test = new Capitalize();
+            var result = test.Execute("a lazy log");
+
+            Assert.AreEqual("A Lazy Log", result);
+        }
+    }
+}
